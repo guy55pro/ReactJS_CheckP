8bcd3d40fc68f2ab8cab9414f87c07ab7020a905
MahdiKrimi <mahdi-smile@hotmail.com>
2021-05-24 06:50:04 -0700

    moi

---
+++ react_cp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './Components/form/Form'
+import NavBar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
